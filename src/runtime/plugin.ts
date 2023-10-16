0d1b80f9cb2161abcf9871767362ab29d9fddc75
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-16 16:00:39 +0530

    refactor: simplify plugin logic

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -3,23 +3,14 @@ import { addRouteMiddleware, defineNuxtPlugin, useRoute, useState } from '#impor
 import { options } from '#build/i18n.options'
 
 export default defineNuxtPlugin(async () => {
-  const { locales } = options
-  const currentLocale = getLocaleFromRoute(useRoute())
-
-  // Set locale from the current route
-  if (currentLocale && locales.includes(currentLocale))
-    useState<string>('locale').value = currentLocale
-
   // Add route middleware to load locale messages for the target route
-  if (process.client) {
-    addRouteMiddleware(
-      'i18n-set-locale',
-      async (to) => {
-        const targetLocale = getLocaleFromRoute(to)
-        if (targetLocale && locales.includes(targetLocale))
-          useState<string>('locale').value = targetLocale
-      },
-      { global: true },
-    )
-  }
+  addRouteMiddleware(
+    'i18n-set-locale',
+    async (to) => {
+      const targetLocale = getLocaleFromRoute(to)
+      if (targetLocale && options.locales.includes(targetLocale))
+        useState<string>('locale').value = targetLocale
+    },
+    { global: true },
+  )
 })
