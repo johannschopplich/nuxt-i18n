8cf9805f01af01203321e52c78fd7cdd7f7f4b98
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-10-17 17:21:11 +0530

    refactor: enforce pre for i18n plugin

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -11,28 +11,42 @@ import { options } from '#build/i18n'
 
 const clean = (str: string) => str.split('-')[0].trim().toLowerCase()
 
-export default defineNuxtPlugin(async () => {
-  // Add route middleware to load locale messages for the target route
-  addRouteMiddleware(
-    'i18n-set-locale',
-    async (to) => {
-      if (to.path === '/' && !useCookie('i18n_redirected').value) {
-        const headerLocale = (useRequestHeaders(['accept-language'])['accept-language'] || '')
-          .split(',').map(l => clean(l.split(';')[0])).filter(l => options.locales.includes(l))[0]
-        const browserLocale = typeof document !== 'undefined'
-          ? navigator.languages.map(clean).filter(l => options.locales.includes(l))[0]
-            || (options.locales.includes(clean(navigator.language)) ? clean(navigator.language) : '')
-          : ''
+export default defineNuxtPlugin({
+  name: 'i18n',
+  enforce: 'pre',
+  async setup() {
+    // Add route middleware to load locale messages for the target route
+    addRouteMiddleware(
+      'i18n-set-locale',
+      async (to) => {
+        if (to.path === '/' && !useCookie('i18n_redirected').value) {
+          const headerLocale = (
+            useRequestHeaders(['accept-language'])['accept-language'] || ''
+          )
+            .split(',')
+            .map(l => clean(l.split(';')[0]))
+            .filter(l => options.locales.includes(l))[0]
 
-        const locale = headerLocale || browserLocale
-        if (locale && locale !== options.defaultLocale)
-          return navigateTo(`/${locale}`)
-      }
+          const browserLocale
+            = typeof document !== 'undefined'
+              ? navigator.languages
+                .map(clean)
+                .filter(l => options.locales.includes(l))[0]
+                || (options.locales.includes(clean(navigator.language))
+                  ? clean(navigator.language)
+                  : '')
+              : ''
 
-      const targetLocale = getLocaleFromRoute(to)
-      if (targetLocale && options.locales.includes(targetLocale))
-        useState<string>('locale').value = targetLocale
-    },
-    { global: true },
-  )
+          const locale = headerLocale || browserLocale
+          if (locale && locale !== options.defaultLocale)
+            return navigateTo(`/${locale}`)
+        }
+
+        const targetLocale = getLocaleFromRoute(to)
+        if (targetLocale && options.locales.includes(targetLocale))
+          useState<string>('locale').value = targetLocale
+      },
+      { global: true },
+    )
+  },
 })
